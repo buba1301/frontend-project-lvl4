466821ed4ce3eec219be087cbfeafb30c1d5f2aa
Boris Muzalevskii <bmuza57@gmail.com>
2021-08-19 15:38:25 +0300

    Add onClick func

--- src/components/Channel/index.jsx
+++ src/components/Channel/index.jsx
@@ -4,14 +4,12 @@ import classnames from 'classnames'
 import './styles.css';
 
 const Channel = ({ id, name, activeChannel, onClick }) => {
-	console.log('CHANNEL COMP', id);
-
 	const classNames = classnames('channelItem', {
 		'activeChannel': id === activeChannel
 	})
 
 	return (
-		<div className={classNames}>{name}</div>
+		<div className={classNames} onClick={onClick(id)}>{name}</div>
 	)
 };
 

c40959c62e9eb992bc811f0acff75239b85c7a7b
Boris Muzalevskii <bmuza57@gmail.com>
2021-08-23 13:57:06 +0300

    Add chat name and users count info

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useContext } from 'react';
-import Context from '../../context';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import './styles.css';
 
 const UserInfo = () => {
-  const userName = useContext(Context);
+  const users = useSelector((state) => state.users);
+  const channels = useSelector((state) => state.channels);
+  const activeChannel = useSelector((state) => state.activeChannel);
+
+  const channelName = channels.find(({ id }) => id === activeChannel).name;
 
   return (
     <div className="user-info">
-      <div className="name">{userName}</div>
+      <div className="name">{channelName}</div>
       <div className="status">
-        <span className="circle" />В сети
+        <span className="circle" />
+        {`${users.length} участников`}
       </div>
     </div>
   );

6a1e53e1a36fe8dbf10ef53dfd036d48dcccca74
Boris Muzalevskii <bmuza57@gmail.com>
2021-08-25 23:03:58 +0300

    Create useScrollDown hook

---
+++ src/hooks/useScrollDown.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+const useScrollDown = (messageEl) => {
+  useEffect(() => {
+    if (messageEl) {
+      messageEl.current.addEventListener('DOMNodeInserted', (event) => {
+        const { currentTarget: target } = event;
+        target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+      });
+    }
+  }, []);
+};
+
+export default useScrollDown;

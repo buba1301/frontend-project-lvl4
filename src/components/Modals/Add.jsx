cb963b50fdc8650073097f6641c0ad5d6ca44fd0
Boris Muzalevskii <bmuza57@gmail.com>
2021-08-30 19:17:45 +0300

    Fix packeges

--- src/components/Modals/Add.jsx
+++ src/components/Modals/Add.jsx
@@ -5,18 +5,25 @@ import { useTranslation } from 'react-i18next';
 import { Modal, FormGroup, FormControl } from 'react-bootstrap';
 import { asyncActions, actions } from '../../slices/index.js';
 import SubmitButton from './SubmitButton.jsx';
+import cn from 'classnames';
 
-const Add = () => {
+const Add = ({ opened }) => {
   const { t } = useTranslation();
+  console.log('Add', opened);
 
   const { hideModal } = actions;
 
   const dispatch = useDispatch();
 
-  const inputRef = useRef();
+  const classNames = cn({
+    show: opened,
+  });
+
+  /* const inputRef = useRef();
+
   useEffect(() => {
     inputRef.current.focus();
-  });
+  }); */
 
   const handleSubmit = async ({ name }, { resetForm, setStatus }) => {
     try {
@@ -38,9 +45,9 @@ const Add = () => {
   });
 
   return (
-    <Modal show onHide={handleModal} centered>
+    <Modal className={classNames} onHide={handleModal} centered>
       <Modal.Header closeButton>
-        <Modal.Title>{t('modal.add.header')}</Modal.Title>
+        <Modal.Title>{t(`modal.${add}.header`)}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <form onSubmit={channel.handleSubmit}>
@@ -50,7 +57,7 @@ const Add = () => {
               type="text"
               placeholder={t('placeholder.channel')}
               required
-              ref={inputRef}
+              // ref={inputRef}
               onChange={channel.handleChange}
               value={channel.values.name}
               isInvalid={!!channel.status}

c347675dc375d8a3b53d62ad626436ce60288f38
Boris Muzalevskii <bmuza57@gmail.com>
2021-08-30 19:15:44 +0300

    Add userAdd async dispatch when new channel add

--- src/init.jsx
+++ src/init.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import io from 'socket.io-client';
-import { actions } from './slices';
+import { asyncActions, actions } from './slices';
 import store from './lib/store';
 import App from './App';
+import { useCookies } from './utils/user';
 
 const socket = io({
   transports: ['websocket'],
@@ -13,12 +14,15 @@ const socket = io({
 export default (gon) => {
   const defaultId = gon.currentChannelId;
 
+  console.log('INIT', gon);
+
   store.dispatch(actions.getDataChannels(gon));
   store.dispatch(actions.getDataUsers(gon));
   store.dispatch(actions.getDataMessages(gon));
   store.dispatch(actions.setActiveChannel(gon.currentChannelId));
 
   // TODO: add userName in message header
+  // TODO: add func delete message
 
   socket.on('newMessage', ({ data }) => {
     const { attributes } = data;
@@ -28,10 +32,12 @@ export default (gon) => {
     const { attributes } = data;
     store.dispatch(actions.addUserSuccess([attributes]));
   });
-  socket.on('newChannel', ({ data }) => {
+  socket.on('newChannel', async ({ data }) => {
     const { attributes, id } = data;
+
     store.dispatch(actions.addChannelSuccess([attributes]));
     store.dispatch(actions.setActiveChannel(id));
+    await store.dispatch(asyncActions.addUser({ activeChannel: data.id, userName: useCookies.get() }));
   });
   socket.on('renameChannel', ({ data }) => {
     store.dispatch(actions.renameChannelSuccess(data));

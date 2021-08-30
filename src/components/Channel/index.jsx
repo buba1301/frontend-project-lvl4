91da14bed61788b5419a9b8b02afda89243d5302
Boris Muzalevskii <bmuza57@gmail.com>
2021-08-30 18:58:42 +0300

    Add logic for delete channel icon

--- src/components/Channel/index.jsx
+++ src/components/Channel/index.jsx
@@ -1,16 +1,23 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import classnames from 'classnames';
 
+import { IoIosTrash } from 'react-icons/io';
+
 import './styles.css';
 
-const Channel = ({ id, name, activeChannel, onClick }) => {
+const Channel = ({ id, name, activeChannel, removable, onClick, onClickDelete, onClickRename }) => {
   const classNames = classnames('channelItem', {
     activeChannel: id === activeChannel,
   });
 
   return (
     <div className={classNames} onClick={onClick(id)}>
-      {name}
+      <div className="nameWrap" onDoubleClick={onClickRename}>
+        {name}
+      </div>
+      {removable && <IoIosTrash data-id={id} size={18} className="remove-channel" onClick={onClickDelete} />}
     </div>
   );
 };
